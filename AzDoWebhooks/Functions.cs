ce9eaff38c54233b73b762da969e470d3890ba82
darren neimke <darren.neimke@gmail.com>
2025-01-27 02:34:47 +0000

    feat: update devcontainer configuration, add CI workflow, and refactor Azure Functions

---
+++ AzDoWebhooks/Functions.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace AzDoWebhooks;
+
+public class Functions(ILogger<Functions> logger)
+{
+    private readonly ILogger<Functions> _logger = logger;
+
+    [Function("HttpExample")]
+    public IActionResult HttpExample([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+
+    [Function("AzureDevOpsWebhook")]
+    public IActionResult AzureDevOpsWebhook([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
+    {
+        _logger.LogInformation("Received webhook request from Azure DevOps.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
