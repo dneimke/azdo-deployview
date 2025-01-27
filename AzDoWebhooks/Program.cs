8f6ce9086ca0e81e81df77c76433ff9354ff4c4a
darren neimke <darren.neimke@gmail.com>
2025-01-27 00:42:24 +0000

    WIP devcontainer configuration

--- AzDoWebhooks/Program.cs
+++ AzDoWebhooks/Program.cs
@@ -5,4 +5,9 @@ var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
+// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
+// builder.Services
+//     .AddApplicationInsightsTelemetryWorkerService()
+//     .ConfigureFunctionsApplicationInsights();
+
 builder.Build().Run();
