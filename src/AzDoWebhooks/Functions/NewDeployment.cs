68410269ebb57560327e07dbd2f0bdf27fc3f09c
darren neimke <darren.neimke@gmail.com>
2025-02-02 01:49:34 +0000

    Adds parsing of detailed request fields into mapped data item

--- src/AzDoWebhooks/Functions/NewDeployment.cs
+++ src/AzDoWebhooks/Functions/NewDeployment.cs
@@ -8,6 +8,7 @@ using Microsoft.Azure.Cosmos;
 using Azure.Identity;
 using FromBodyAttribute = Microsoft.Azure.Functions.Worker.Http.FromBodyAttribute;
 using Microsoft.Azure.Functions.Worker.Http;
+using System.Text.RegularExpressions;
 
 namespace AzDoWebhooks.Functions;
 
@@ -24,42 +25,60 @@ public class NewDeployment
 
     [Function("NewDeployment")]
     public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req,
-        [FromBody] DeploymentRequest azureDeployment)
+        [FromBody] DeploymentRequest deployRequest)
     {
         _logger.LogInformation("Received webhook request from Azure DevOps.");
         _logger.LogInformation(_configuration["CosmosDbConnection"]);
 
-        CosmosClient client = new(
-            accountEndpoint: _configuration["CosmosDbConnection"],
-            tokenCredential: new DefaultAzureCredential()
-        );
+        string pattern = @"Deployment of release (?<ReleaseName>[\w-]+) on stage (?<StageName>\w+) (?<Status>\w+)\. Time to deploy: (?<DeployTime>\d+\.\d+) minutes\.";
+        Match match = Regex.Match(deployRequest.DetailedMessage.Text, pattern);
 
-        var database = client.GetDatabase("azdodeploy-db");
-        var container = database.GetContainer("deployments");
-
-        DeploymentResponse deployment = new()
+        if (match.Success)
         {
-            Environment = azureDeployment.SubscriptionId.ToString(),
-            Status = azureDeployment.EventType,
-            DeploymentTime = azureDeployment.CreatedDate.Date,
-            Project = azureDeployment.Message.Text,
-            partitionKey = azureDeployment.Message.Text
-        };
 
-        try
-        {
-            var response = await container.UpsertItemAsync<DeploymentResponse>(
-                item: deployment,
-                partitionKey: new PartitionKey(deployment.Project)
-            );
+            var releaseName = match.Groups["ReleaseName"].Value;
+            var stageName = match.Groups["StageName"].Value;
+            var status = match.Groups["Status"].Value;
+            var deployTime = match.Groups["DeployTime"].Value;
+            var projectId = deployRequest.ResourceContainers.Project.Id.ToString();
+
+            DeploymentResponse deployment = new()
+            {
+                EventType = deployRequest.EventType,
+                ReleaseName = releaseName,
+                Environment = stageName,
+                Status = status,
+                DeploymentDateTime = deployRequest.CreatedDate.Date,
+                DeploymentDuration = deployTime,
+                Project = projectId,
+                Message = deployRequest.DetailedMessage.Text,
+                partitionKey = projectId
+            };
+
+            try
+            {
+                CosmosClient client = new(
+                    accountEndpoint: _configuration["CosmosDbConnection"],
+                    tokenCredential: new DefaultAzureCredential()
+                );
+
+                var database = client.GetDatabase("azdodeploy-db");
+                var container = database.GetContainer("deployments");
+
+                var response = await container.UpsertItemAsync<DeploymentResponse>(
+                    item: deployment,
+                    partitionKey: new PartitionKey(deployment.Project)
+                );
+
+                return new OkObjectResult($"Successfully added new deployment {deployment.id}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("An error occurred while attempting to add {deploymentId}. {exceptionMessage}", deployment.id, ex.ToString());
+            }
         }
-        catch (Exception ex)
-        {
-            _logger.LogError("An error occurred while attempting to add {deploymentId}. {exceptionMessage}", deployment.id, ex.ToString());
-        }
 
-
-        return new OkObjectResult($"Successfully added new deployment {deployment.id}");
+        return new BadRequestResult();
     }
 }
 
--- src/AzDoWebhooks/Models/Deployment.cs
+++ src/AzDoWebhooks/Models/Deployment.cs
@@ -3,9 +3,13 @@ namespace AzDoWebhooks.Models;
 public class DeploymentResponse
 {
     public Guid id { get; set; } = Guid.NewGuid();
+    public string EventType { get; set; } = "";
+    public string ReleaseName { get; set; } = "";
     public string Environment { get; set; } = "";
     public string Project { get; set; } = "";
     public string Status { get; set; } = "";
-    public DateTime DeploymentTime { get; set; }
+    public DateTime DeploymentDateTime { get; set; }
+    public string DeploymentDuration { get; set; } = "";
+    public string Message { get; set; } = "";
     public string partitionKey { get; set; } = "";
 }
